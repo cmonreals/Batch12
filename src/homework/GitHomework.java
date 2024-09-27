cc7eab2bfc85107a4545f8d4c70a18b8905171f9
carlo <carlo@LAPTOP-D10ISHDO.attlocal.net>
2024-09-26 19:20:34 -0600

    add display

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -1,13 +1,8 @@
 package homework;
 
 public class GitHomework {
-	public void getCity() {
-		System.out.println("Reston");
-	}
-	public void GetCountry() {
-		System.out.println ("USA");
-	}
-	//added this code
-	int a = 10;
-	int b = 8;
+	public void display() {
+		System.out.println ("John Cena");
+		
+		}
 }
