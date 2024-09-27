5098d27efdc318cda8a143c12bdb05d09c7b2e7a
carlo <carlo@LAPTOP-D10ISHDO.attlocal.net>
2024-09-26 18:19:55 -0600

    added new method

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -4,4 +4,9 @@ public class GitHomework {
 	public static void main (String[]args) {
 		System.out.println("Batch12: Git Class Lecture");
 	}
+	
+	
+	public static void add (int a , int b) {
+		System.out.println(a+b);
+	}
 }
