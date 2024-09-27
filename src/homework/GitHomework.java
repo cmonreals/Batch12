a6b282a134146689375a2082033f803df407e594
carlo <carlo@LAPTOP-D10ISHDO.attlocal.net>
2024-09-26 18:57:17 -0600

    delete code

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -1,15 +1,5 @@
 package homework;
 
 public class GitHomework {
-	public static void main (String[]args) {
-		System.out.println("Batch12: Git Class Lecture");
-		int age= 12; 
-		String name = "John";
-		//This comes from Github
-	}
 	
-	
-	public static void add (int a , int b) {
-		System.out.println(a+b);
-	}
 }
