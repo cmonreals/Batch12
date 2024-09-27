7f63233edef4c77be64a6f09de613ccf6dc2a9e3
cmonreals <carlosmonreals@gmail.com>
2024-09-26 19:25:27 -0600

    Update GitHomework.java

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -4,5 +4,12 @@ public class GitHomework {
 	public void display() {
 		System.out.println ("John Cena");
 		
-		}
+	}
+	/**
+
+	THid id from Github
+ 	Java is fun
+ 	GIT is easy
+  	API is not fun 
+	*/
 }
