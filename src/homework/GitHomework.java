f0f7c2ec0a13c82fbb599d85dd887d9595fb5c2c
carlo <carlo@LAPTOP-D10ISHDO.attlocal.net>
2024-09-26 19:35:22 -0600

    1

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -1,15 +1,6 @@
 package homework;
 
 public class GitHomework {
-	public void display() {
-		System.out.println ("John Cena");
+	
 		
-	}
-	/**
-
-	THid id from Github
- 	Java is fun
- 	GIT is easy
-  	API is not fun 
-	*/
 }
