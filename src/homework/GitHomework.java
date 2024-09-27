c76b18df57eea89c1d169455e7d9329dfcbb6355
carlo <carlo@LAPTOP-D10ISHDO.attlocal.net>
2024-09-26 19:14:56 -0600

    add cahnges

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -1,5 +1,13 @@
 package homework;
 
 public class GitHomework {
-	
+	public void getCity() {
+		System.out.println("Reston");
+	}
+	public void GetCountry() {
+		System.out.println ("USA");
+	}
+	//added this code
+	int a = 10;
+	int b = 8;
 }
